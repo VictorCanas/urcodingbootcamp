269474c6ea581becb4f383635cc5093d78dd7715
Victor <victorcanasint@gmail.com>
2018-02-06 19:08:37 -0500

    express calculator

--- week-14/01-Activities/01-ExpressCalculator/Unsolved/expressCalculator.js
+++ week-14/01-Activities/01-ExpressCalculator/Unsolved/expressCalculator.js
@@ -9,13 +9,17 @@ var app = express();
 // Routes
 // What routes do you need to have? Which ones are optional?
 // TODO Add your routes here
-app.get("/:operator/:num1/:num2", function(req, res) {
+
+//give it three wildcards
+app.get("/:operation/:num1/:num2", function(req, res) {
 
   // TODO parse out the variables from the request
   // Parameters are received from the URL
+  let operation = req.params.operation;
   // TODO make sure they're converted to integers (and not strings)
+  let n1 = parseInt(req.params.num1);
   // Parameters are converted to integers
-
+  let n2 = parseInt(req.params.num2)
   // Initialize the result variable to send later
   var result;
   // Switch statement chooses operation based on the operation parameter.
@@ -23,15 +27,19 @@ app.get("/:operator/:num1/:num2", function(req, res) {
     // BONUS - How could you use * + etc. inside the app.get()?
     case "add":
       // Add your logic here. Pun intended.
+      result = n1 + n2;
       break;
     case "subtract":
       // Subtract logic
+      result = n1 - n2;
       break;
     case "multiply":
       // Multiply
+      result = n1 * n2;
       break;
     case "divide":
       // Divide
+      result = n1 / n2;
       break;
     default:
       // Handle anything that isn't specified
