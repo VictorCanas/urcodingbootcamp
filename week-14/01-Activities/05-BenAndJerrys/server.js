879b1e6a400609914d622198b677486a87a6e061
Victor <victorcanasint@gmail.com>
2018-02-08 19:07:20 -0500

    solution

--- week-14/01-Activities/05-BenAndJerrys/server.js
+++ week-14/01-Activities/05-BenAndJerrys/server.js
@@ -13,29 +13,26 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Data
- var icecreams = [
-      {name: 'vanilla', price: 10, awesomeness: 3},
-      {name: 'chocolate', price: 4, awesomeness: 8},
-      {name: 'banana', price: 1, awesomeness: 1},
-      {name: 'greentea', price: 5, awesomeness: 7},
-      {name: 'jawbreakers', price: 6, awesomeness: 2},
-    ];
-
+var icecreams = [
+  { name: "vanilla", price: 10, awesomeness: 3 },
+  { name: "chocolate", price: 4, awesomeness: 8 },
+  { name: "banana", price: 1, awesomeness: 1 },
+  { name: "greentea", price: 5, awesomeness: 7 },
+  { name: "jawbreakers", price: 6, awesomeness: 2 },
+  { name: "vanilla", price: 10, awesomeness: 3 }
+];
 
 // Routes
 app.get("/icecream/:name", function(req, res) {
-  res.render("index", icecreams[0]);
+  for (var i = 0; i < icecreams.length; i++) {
+    if (icecreams[i].name === req.params.name) {
+      return res.render("icecream", icecreams[i]);
+    }
+  }
 });
 
 app.get("/icecreams", function(req, res) {
-  res.render("index", lunches[1]);
-});
-
-app.get("/lunches", function(req, res) {
-  res.render("all-lunches", {
-    foods: lunches,
-    eater: "david"
-  });
+  res.render("ics", { ics: icecreams });
 });
 
 // Initiate the listener.
