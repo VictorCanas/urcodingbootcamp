13a2f84b77bb97402df5533b581c998a6eac0773
Victor <victorcanasint@gmail.com>
2018-03-22 21:22:16 -0400

    react

--- week-19/01-Activities/15-Stu_DecrementCounter/Unsolved/src/components/Counter.js
+++ week-19/01-Activities/15-Stu_DecrementCounter/Unsolved/src/components/Counter.js
@@ -2,11 +2,20 @@ import React from "react";
 
 class Counter extends React.Component {
   state = {
-    count: 0
+    count: 0,
+    poop: ""
   };
 
-  handleIncrement = () => {
+  Increment = () => {
     this.setState({ count: this.state.count + 1 });
+  };
+
+  Decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  Escrement = () => {
+    this.setState({ poop: this.state.poop + '💩' });
   };
 
   render() {
@@ -17,8 +26,15 @@ class Counter extends React.Component {
         </div>
         <div className="card-body">
           <p className="card-text">Click Count: {this.state.count}</p>
-          <button type="button" className="btn btn-primary" onClick={this.handleIncrement}>
+          <p className="card-text">Escrement {this.state.poop}</p>
+          <button type="button" className="btn btn-success" style={{ margin: "5px" }} onClick={this.Increment}>
             Increment
+          </button>
+          <button type="button" className="btn btn-danger" style={{ margin: "5px" }} onClick={this.Decrement}>
+            Decrement
+          </button>
+          <button type="button" className="btn btn-warning" style={{ margin: "5px" }} onClick={this.Escrement}>
+            Escrement
           </button>
         </div>
       </div>
